c318947314a6855808cc0351a2a7f57a2c4bdaeb
Kay Arnesen <subzero7748@gmail.com>
2017-08-30 16:54:18 +0200

    Copy FIRMWARE.BIN to out dir

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,4 @@
 add_lunch_combo lineage_grandppltedx-userdebug
-
+cp kernel/samsung/grandppltedx/firmware/tsp_zinitix/FIRMWARE.BIN kernel/samsung/grandppltedx/firmware/tsp_zinitix/bt541_GP.fw
+mkdir -p out/target/product/grandppltedx/obj/KERNEL_OBJ/firmware/tsp_zinitix/
+cp kernel/samsung/grandppltedx/firmware/tsp_zinitix/FIRMWARE.BIN out/target/product/grandppltedx/obj/KERNEL_OBJ/firmware/tsp_zinitix/bt541_GP.fw
