23c5a869247909104cc7fc336c3fd06753be0974
Tim Lin <timtmlin@google.com>
2019-01-22 17:17:51 +0800

    grandppltedx: libril: set initial value to GW timingAdvance
    RIL_SignalStrength_v10 not support gw.timingAdvance. But default 0 will
    be taken as valid value. Set to INT_MAX as invalid value.
    
    Bug: 123096279
    Test: Build pass. Correct value for GW timingAdvance in radio log.
    01-22 16:22:41.150  1779  1910 V RILJ    : [UNSL]< UNSOL_SIGNAL_STRENGTH
    SignalStrength:{ mGsm=CellSignalStrengthGsm: rssi=2147483647 ber=99 mTa=2147483647
    
    [haggertk: Upstream commit updated to patch both
     convertRilSignalStrengthToHalV8 and convertRilSignalStrengthToHalV10,
     as the upstream libril only has a single convertRilSignalStrengthToHal]
    
    Change-Id: I37cc2c246d045a07ffad863fb0cc852d8184c3ca

--- ril/libril/ril_service.cpp
+++ ril/libril/ril_service.cpp
@@ -7159,6 +7159,10 @@ void convertRilSignalStrengthToHalV8(void *response, size_t responseLen,
 
     signalStrength.gw.signalStrength = rilSignalStrength->GW_SignalStrength.signalStrength;
     signalStrength.gw.bitErrorRate = rilSignalStrength->GW_SignalStrength.bitErrorRate;
+    // RIL_SignalStrength_v10 not support gw.timingAdvance. Set to INT_MAX as
+    // invalid value.
+    signalStrength.gw.timingAdvance = INT_MAX;
+
     signalStrength.cdma.dbm = rilSignalStrength->CDMA_SignalStrength.dbm;
     signalStrength.cdma.ecio = rilSignalStrength->CDMA_SignalStrength.ecio;
     signalStrength.evdo.dbm = rilSignalStrength->EVDO_SignalStrength.dbm;
@@ -7202,6 +7206,10 @@ void convertRilSignalStrengthToHalV10(void *response, size_t responseLen,
 
     signalStrength.gw.signalStrength = rilSignalStrength->GW_SignalStrength.signalStrength;
     signalStrength.gw.bitErrorRate = rilSignalStrength->GW_SignalStrength.bitErrorRate;
+    // RIL_SignalStrength_v10 not support gw.timingAdvance. Set to INT_MAX as
+    // invalid value.
+    signalStrength.gw.timingAdvance = INT_MAX;
+
     signalStrength.cdma.dbm = rilSignalStrength->CDMA_SignalStrength.dbm;
     signalStrength.cdma.ecio = rilSignalStrength->CDMA_SignalStrength.ecio;
     signalStrength.evdo.dbm = rilSignalStrength->EVDO_SignalStrength.dbm;
