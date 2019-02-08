b89e488fb47fcb0c263d31d7e7ea8eab4f6d1470
Anton Hansson <hansson@google.com>
2019-02-08 17:56:34 +0000

    grandppltedx: Use generic arch variant
    Follow blueline which has not demonstrated any regressions
    after switching to a generic arch variant.
    
    Bug: 120773446
    Test: make
    Change-Id: I9e23963fcfa5ccaa912f2930a632458c4d2a692d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,7 +85,7 @@ TARGET_ARCH		:= arm
 TARGET_ARCH_VARIANT	:= armv8-a
 TARGET_CPU_ABI		:= armeabi-v7a
 TARGET_CPU_ABI2		:= armeabi
-TARGET_CPU_VARIANT	:= cortex-a53
+TARGET_CPU_VARIANT	:= generic
 TARGET_CPU_SMP		:= true
 
 TARGET_2ND_ARCH		:= 
