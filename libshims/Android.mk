cce00db48fd431cfe980958336b6ed46a522a2c4
Almond <iwanttobebetter7@gmail.com>
2022-02-23 12:04:48 +0700

    shim: move to respective folder(s)
    Signed-off-by: Almond

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 # ifc*
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := thermal_shim.cpp
-LOCAL_MODULE := libshim_thermal
+LOCAL_MODULE := ifc/libshim_thermal
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
@@ -11,7 +11,7 @@ include $(BUILD_SHARED_LIBRARY)
 # Parcel
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := ril_shim.cpp
-LOCAL_MODULE := libshim_ril
+LOCAL_MODULE := ril/libshim_ril
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
@@ -19,7 +19,7 @@ include $(BUILD_SHARED_LIBRARY)
 # Cam and FILEPATHE
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := libbinder liblog libgui libui libicuuc libicui18n libmedia
-LOCAL_SRC_FILES := camera_shim.c mtk_cam.cpp
+LOCAL_SRC_FILES := camera/camera_shim.c camera/mtk_cam.cpp
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -30,7 +30,7 @@ include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := external/boringssl/include
 LOCAL_SHARED_LIBRARIES := liblog
 
-LOCAL_SRC_FILES := mtkagpsd_shim.c
+LOCAL_SRC_FILES := agps/mtkagpsd_shim.c
 LOCAL_MODULE := libshim_agpsd
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -40,7 +40,7 @@ include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := liblog
 
 # XLOG for everything MTK
-LOCAL_SRC_FILES := xlog_shim.c
+LOCAL_SRC_FILES := xlog/xlog_shim.c
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_MODULE := libshim_xlog
 LOCAL_MODULE_TAGS := optional
