6c107839d2a24a5037059a413a221907a8892a3a
Almond <iwanttobebetter7@gmail.com>
2022-03-11 09:49:48 +0700

    audio: fiddling with policy
    idk what i'm doing help
    
    Signed-off-by: Almond

--- device.mk
+++ device.mk
@@ -62,11 +62,8 @@ PRODUCT_PACKAGES += \
 	libtinymix \
 	libtinyxml
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,frameworks/av/services/audiopolicy/config,system/etc)
 PRODUCT_COPY_FILES += \
-	frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
-	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
-	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
 	frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
