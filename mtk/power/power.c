63933f75ef4b23c74ae8ab5d4f9bde4b1adf7f0d
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-04-11 23:15:26 +0700

    grandppltedx: Update Power HAL for Pie
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- mtk/power/power.c
+++ mtk/power/power.c
@@ -1,8 +1,8 @@
 /*
- * Copyright 2017 The Android Open Source Project
+ * Copyright (C) 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * You may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
@@ -12,137 +12,110 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
- * This contains the module build definitions for the hardware-specific
- * components for this device.
- * As much as possible, those components should be built unconditionally,
- * with device-specific names to avoid collisions, to avoid device-specific
- * bitrot and build breakages. Building a component unconditionally does
- * *not* include it on all devices, so it is safe even with hardware-specific
- * components.
  */
+
 #include <errno.h>
-#include <fcntl.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-#define LOG_TAG "MTK PowerHAL"
+#define LOG_TAG "MTKPowerHAL"
 #include <utils/Log.h>
 
-#include <cutils/properties.h>
-
+#include <hardware/hardware.h>
 #include <hardware/power.h>
 
-enum {
-    PROFILE_POWER_SAVE,
-    PROFILE_BALANCED,
-    PROFILE_HIGH_PERFORMANCE,
-    PROFILE_BIAS_POWER, 
-};
-
-#define POWER_NR_OF_SUPPORTED_PROFILES 4
-											   
-#define POWER_PROFILE_PROPERTY  "sys.perf.profile"
-#define POWER_SAVE_PROP         "0"
-#define BALANCED_PROP           "1"
-#define HIGH_PERFORMANCE_PROP   "2"
-#define BIAS_POWER_PROP         "3"
-
-static int current_power_profile = PROFILE_BALANCED;
-
-static int sysfs_write(const char *path, char *s)
+static void power_init(struct power_module *module)
 {
-    char buf[80];
-    int len;
-    int fd = open(path, O_WRONLY);
-
-    if (fd < 0) {
-        strerror_r(errno, buf, sizeof(buf));
-        ALOGE("Error opening %s: %s\n", path, buf);
-        return -1;
-    }
-
-    len = write(fd, s, strlen(s));
-    if (len < 0) {
-        strerror_r(errno, buf, sizeof(buf));
-        ALOGE("Error writing to %s: %s\n", path, buf);
-        return -1;
-    }
-
-    close(fd);
-    return 0;
+	if (module)
+		ALOGI("%s", __func__);
 }
 
-static void power_init(struct power_module *module __unused)
-{
-    ALOGI("%s", __func__);
+static void power_set_interactive(struct power_module *module __unused, int on __unused) {}
+
+int sysfs_write(char* path, char* s) {
+	char buf[80];
+	int len;
+	int ret = 0;
+	int fd = open(path, O_WRONLY);
+
+	if (fd < 0) {
+		strerror_r(errno, buf, sizeof(buf));
+		ALOGE("Error opening %s: %s\n", path, buf);
+		return -1;
+	}
+
+	len = write(fd, s, strlen(s));
+	if (len < 0) {
+		strerror_r(errno, buf, sizeof(buf));
+		ALOGE("Error writing to %s: %s\n", path, buf);
+		ret = -1;
+	}
+	close(fd);
+
+	return ret;
 }
 
-static void power_set_interactive(struct power_module *module __unused,
-                int on __unused)
+static void power_set_feature(struct power_module *module __unused, feature_t feature, int state)
 {
+#ifdef TAP_TO_WAKE_NODE
+	if (feature == POWER_FEATURE_DOUBLE_TAP_TO_WAKE)
+		sysfs_write(TAP_TO_WAKE_NODE, state ? "1" : "0");
+#endif
 }
 
-static void set_power_profile(int profile)
-{
-    if (profile == current_power_profile)
-        return;
+static void power_hint(struct power_module *module __unused, power_hint_t hint __unused, void *data __unused) {}
 
-    switch (profile) {
-    case PROFILE_POWER_SAVE:
-        property_set(POWER_PROFILE_PROPERTY, POWER_SAVE_PROP);
-        break;
-    case PROFILE_BALANCED:
-        property_set(POWER_PROFILE_PROPERTY, BALANCED_PROP);
-        break;
-    case PROFILE_HIGH_PERFORMANCE:
-        property_set(POWER_PROFILE_PROPERTY, HIGH_PERFORMANCE_PROP);
-        break;
-    case PROFILE_BIAS_POWER:
-        property_set(POWER_PROFILE_PROPERTY, BIAS_POWER_PROP);
-        break;
-    }
+static int power_open(const hw_module_t* module, const char* name, hw_device_t** device) {
+	if (module)
+		ALOGI("%s: enter; name=%s", __FUNCTION__, name);
 
-    current_power_profile = profile;
-}
+	int retval = 0; /* 0 is ok; -1 is error */
 
-static void power_hint(struct power_module *module __unused, power_hint_t hint,
-                void *data __unused)
-{
-    if (hint == POWER_HINT_SET_PROFILE)
-        set_power_profile(*(int32_t *)data);
-	  
-}
+	if (strcmp(name, POWER_HARDWARE_MODULE_ID) == 0) {
+		power_module_t *dev = (power_module_t *)calloc(1, sizeof(power_module_t));
 
-static void set_feature(struct power_module *module __unused,
-                feature_t feature, int state)
-{
-}
+		if (dev) {
+			/* Common hw_device_t fields */
+			dev->common.tag = HARDWARE_DEVICE_TAG;
+			dev->common.module_api_version = POWER_MODULE_API_VERSION_0_3;
+			dev->common.hal_api_version = HARDWARE_HAL_API_VERSION;
 
-static int get_feature(struct power_module *module __unused, feature_t feature)
-{
-    if (feature == POWER_FEATURE_SUPPORTED_PROFILES)
-        return POWER_NR_OF_SUPPORTED_PROFILES;
-    return -1;
+			dev->init = power_init;
+			dev->powerHint = power_hint;
+			dev->setFeature = power_set_feature;
+			dev->setInteractive = power_set_interactive;
+
+			*device = (hw_device_t*)dev;
+		} else
+			retval = -ENOMEM;
+	} else
+		retval = -EINVAL;
+
+	ALOGD("%s: exit %d", __FUNCTION__, retval);
+	return retval;
 }
 
 static struct hw_module_methods_t power_module_methods = {
-    .open = NULL,
+	.open = power_open
 };
 
 struct power_module HAL_MODULE_INFO_SYM = {
-    .common = {
-        .tag = HARDWARE_MODULE_TAG,
-        .module_api_version = POWER_MODULE_API_VERSION_0_3,
-        .hal_api_version = HARDWARE_HAL_API_VERSION,
-        .id = POWER_HARDWARE_MODULE_ID,
-        .name = "MTK Power HAL",
-        .author = "The Android Open Source Project",
-        .methods = &power_module_methods,
-    },
-    .init = power_init,
-    .powerHint = power_hint,
-    .setInteractive = power_set_interactive,
-    .setFeature = set_feature,
-    .getFeature = get_feature
+	.common = {
+		.tag = HARDWARE_MODULE_TAG,
+		.module_api_version = POWER_MODULE_API_VERSION_0_3,
+		.hal_api_version = HARDWARE_HAL_API_VERSION,
+		.id = POWER_HARDWARE_MODULE_ID,
+		.name = "Mediatek Power HAL",
+		.author = "The Android Open Source Project",
+		.methods = &power_module_methods
+	},
+
+	.init = power_init,
+	.setInteractive = power_set_interactive,
+	.setFeature = power_set_feature,
+	.powerHint = power_hint
 };
