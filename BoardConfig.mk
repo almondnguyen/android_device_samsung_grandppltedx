0e790d00d91be5b8c7c21b008067f4c29ec210c4
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-04-12 21:55:44 +0700

    grandppltedx: Add device compatibility matrix.
    Add device compatibility matrix to common project and
    targets can point the build variable DEVICE_MATRIX_FILE to
    this file to build device compatibility matrix.
    
    Change-Id: If8e75c5ee8de0ef50d6937ffbd6453fb3f5eb648
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -276,6 +276,7 @@ WITHOUT_CHECK_API := true
 
 # Manifest
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/compatibility_matrix.xml
 
 # Misc
 EXTENDED_FONT_FOOTPRINT := true
