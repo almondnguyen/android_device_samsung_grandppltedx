810272c4495ba537f8da49b6c29247274e229873
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-04-16 21:02:40 +0700

    grandppltedx: Initial proprietary files list
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

---
+++ setup-makefiles.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+# Required!
+DEVICE=grandppltedx
+VENDOR=samsung
+
+INITIAL_COPYRIGHT_YEAR=2023
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../.."
+
+HELPER="${ANDROID_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+
+# Copyright headers and guards
+write_headers "Lumi"
+
+# The standard common blobs
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+# Finish
+write_footers
