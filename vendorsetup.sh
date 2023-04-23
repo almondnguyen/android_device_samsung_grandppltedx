c6817e91cba55b9fbd59ec2e4e6ebeed211c7004
Almond <42428221+almondnguyen@users.noreply.github.com>
2023-04-23 21:47:38 +0700

    vendorsetup: cleanup
    Signed-off-by: Almond <42428221+almondnguyen@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,12 +3,7 @@
 # Your mileage may vary
 export LC_ALL=C
 
-# recommended by Lineage
-# sometimes not work, do it in ~/.profile
 #export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
 
-# 8GB-dual
 add_lunch_combo lineage_grandppltedx-userdebug
 add_lunch_combo lineage_grandppltedx-eng
-
-# 16GB-single
