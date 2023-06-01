359fc7713a165dc563c486d38310072a85d4d777
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-06-01 16:46:00 +0700

    grandppltedx: Build Bluetooth HAL
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- device.mk
+++ device.mk
@@ -129,6 +129,9 @@ PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
 #-- BT
+PRODUCT_PACKAGES += \
+	android.hardware.bluetooth@1.0-impl
+
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.bluetooth_le.xml \
 	frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.bluetooth.xml
