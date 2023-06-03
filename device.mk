55d47e564e7569fb6f18f76b69ed791c6c37a7e7
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-06-03 19:24:15 +0700

    grandppltedx: Uprev Health HAL to 2.0
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- device.mk
+++ device.mk
@@ -262,8 +262,8 @@ PRODUCT_PACKAGES += android.hardware.memtrack@1.0-impl
 
 # Health HAL
 PRODUCT_PACKAGES += \
-	android.hardware.health@1.0-impl \
-	android.hardware.health@1.0-service
+	android.hardware.health@2.0-impl \
+	android.hardware.health@2.0-service
 
 # Rootdir
 PRODUCT_PACKAGES += \
