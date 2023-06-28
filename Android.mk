9c2fdac544439a95b6d50daa0ac45408f64f0c2c
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-06-28 17:36:05 +0700

    grandppltedx: Update copyright
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,9 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 LOCAL_PATH := device/samsung/grandppltedx
 
 ifneq ($(filter grandppltedx,$(TARGET_DEVICE)),)
