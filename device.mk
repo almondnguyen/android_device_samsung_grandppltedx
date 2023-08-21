82044621bf0d2987699245a30c89aa4d7fb46a99
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-08-21 22:27:49 +0700

    Revert "grandppltedx: Binderize composer"
    This reverts commit 6b4df3c357bea7b78773fd76612899e894b8a2e7.

--- device.mk
+++ device.mk
@@ -41,8 +41,7 @@ PRODUCT_PACKAGES += \
 	android.hardware.graphics.allocator@2.0-impl \
 	android.hardware.graphics.allocator@2.0-service \
 	android.hardware.graphics.mapper@2.0-impl \
-	android.hardware.graphics.composer@2.1-impl \
-	android.hardware.graphics.composer@2.1-service
+	android.hardware.graphics.composer@2.1-impl
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3
