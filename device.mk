a434fb88f07148dceb34e08ffb305dad513bf1b6
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-08-23 17:32:54 +0700

    grandppltedx: Binderize keymaster
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- device.mk
+++ device.mk
@@ -257,7 +257,9 @@ PRODUCT_PACKAGES += \
 	android.hardware.light@2.0-service.samsung
 
 # Keymaster
-PRODUCT_PACKAGES += android.hardware.keymaster@3.0-impl
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service \
 
 # Memtrack
 PRODUCT_PACKAGES += android.hardware.memtrack@1.0-impl
