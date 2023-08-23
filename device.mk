d8eae0a5019c52f094115622da56779896bf521d
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-08-23 18:36:26 +0700

    grandppltedx: Build Gatekeeper HIDL
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- device.mk
+++ device.mk
@@ -256,6 +256,11 @@ PRODUCT_PACKAGES += \
 	lights.mt6737t \
 	android.hardware.light@2.0-service.samsung
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service \
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
