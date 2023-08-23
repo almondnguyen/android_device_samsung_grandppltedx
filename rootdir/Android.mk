1e43d4ec6f940db091ec5785ab2d8374d1b50425
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-08-23 22:07:41 +0700

    grandppltedx: rootdir: Properly define targets
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,23 +1,81 @@
 LOCAL_PATH := device/samsung/grandppltedx/rootdir
 
-define add-rootdir-targets
-$(foreach target,$(1), \
-   $(eval include $(CLEAR_VARS)) \
-   $(eval LOCAL_MODULE       := $(target)) \
-   $(eval LOCAL_MODULE_CLASS := ETC) \
-   $(eval LOCAL_SRC_FILES    := $(target)) \
-   $(eval LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)) \
-   $(eval include $(BUILD_PREBUILT)))
-endef
+include $(CLEAR_VARS)
+LOCAL_MODULE            := fstab.mt6735
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := fstab.mt6735
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
 
-$(call add-rootdir-targets, \
-	fstab.mt6735 \
-	init.modem.rc \
-	init.mt6735.rc \
-	init.mt6735.usb.rc \
-	init.project.rc \
-	init.recovery.mt6735.rc \
-	init.wifi.rc \
-	ueventd.mt6735.rc \
-	init.xlog.rc \
-	init.samsung.rc)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.modem.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := init.modem.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.mt6735.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := init.mt6735.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.mt6735.usb.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := init.mt6735.usb.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.project.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := init.project.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.recovery.mt6735.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := init.recovery.mt6735.rc
+LOCAL_MODULE_PATH       := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.wifi.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := init.wifi.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := ueventd.mt6735.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := ueventd.mt6735.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.xlog.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := init.xlog.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.samsung.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := init.samsung.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
