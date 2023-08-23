8e7bc9f7c13852b66411bc892a5a8d322bbd786b
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-08-23 22:34:43 +0700

    grandppltedx: Binderize camera
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- device.mk
+++ device.mk
@@ -221,7 +221,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
 	Snap \
-	android.hardware.camera.provider@2.4-impl-legacy
+	android.hardware.camera.provider@2.4-impl \
+	android.hardware.camera.provider@2.4-service
 
 #-- perm
 PRODUCT_COPY_FILES += \
