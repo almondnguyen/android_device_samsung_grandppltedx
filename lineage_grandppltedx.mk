db3e16d4dcc36b2b6c644a3672b8cd91d1d7f789
Almond <42428221+almondnguyen@users.noreply.github.com>
2023-09-13 00:08:02 +0700

    grandpplte: use go_default config
    Signed-off-by: Almond <42428221+almondnguyen@users.noreply.github.com>

--- lineage_grandppltedx.mk
+++ lineage_grandppltedx.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
