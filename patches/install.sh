9c4bd2d208171d8368f6e0b8fa3dbe29fa961ddd
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-11-14 13:29:21 +0700

    patches: Drop bionic patches
    - See a68c08334f10d711917feb73adea5bad94b3ad9d.

--- patches/install.sh
+++ patches/install.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="bionic frameworks/av frameworks/base frameworks/native system/core"
+dirs="frameworks/av frameworks/base frameworks/native system/core"
 
 for dir in $dirs ; do
 	cd $rootdirectory
